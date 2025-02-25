f4a312bd03ffd8d2f980758b80e809d37ccf00b2
muskanmorshed <muskan.morshed@ontariotechu.net>
2025-02-25 18:59:47 -0500

    backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './routes/user-routes.js';
+
+const app = express();
+
+app.use("/api/user", router); // optional add next to this, http://localhost:3000/api/user/logon
+mongoose.connect(
+    "mongodb+srv://Fashionista:Pixelate.123@cluster0.8kogq.mongodb.net/FashionRetail?retryWrites=true&w=majority&appName=Cluster0"
+).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => console.log("Server running on port 3000"));
+}).catch((err) => console.log(err));
